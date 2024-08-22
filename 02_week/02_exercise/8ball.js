cc1075f44720b8bff2804e86b71e742f52761fdf
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-08-22 13:47:13 +0300

    add 8-ball task

---
+++ 02_week/02_exercise/8ball.js
@@ -0,0 +1,1 @@
+// Create a Magic 8-Ball program that emulates the classic fortune-telling toy. The user will input a yes-or-no question, and the program will output a random response from the 8-Ball. Implement the program using conditional statements to select and display one of several predefined responses.
