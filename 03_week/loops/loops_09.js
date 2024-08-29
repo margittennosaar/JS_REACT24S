0fe94f7257d6268239427439afd5b15714ceebb7
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-08-29 11:49:18 +0300

    add loop tasks 9-10

---
+++ 03_week/loops/loops_09.js
@@ -0,0 +1,2 @@
+// Make program which will use a while loop to calculate the sum of all numbers from 1 to 100.
+
---
+++ 03_week/loops/loops_10.js
@@ -0,0 +1,2 @@
+// Make a programm which will take in a string as an argument and will reverse it.
+
