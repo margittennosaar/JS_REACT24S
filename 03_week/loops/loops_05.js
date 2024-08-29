f802cad4d68fe93c401b362880c17454c79f83be
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-08-29 09:57:08 +0300

    add loop tasks 5-8

---
+++ 03_week/loops/loops_05.js
@@ -0,0 +1,1 @@
+// Develop a program that continuously prompts the user to input numbers until the user enters 0, at which point the program terminates. After termination, the program should calculate and display the average of all the entered numbers.
---
+++ 03_week/loops/loops_07.js
@@ -0,0 +1,1 @@
+/* Make a program that asks ten numbers. Program calculates and prints out sum and average, also prints out the smallest and biggest number. */
