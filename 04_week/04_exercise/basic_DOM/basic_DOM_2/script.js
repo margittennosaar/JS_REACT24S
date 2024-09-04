849b8c4a7d21ea7c432fe3a9587193b62faad039
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-09-04 13:53:22 +0300

    add second DOM task

---
+++ 04_week/04_exercise/basic_DOM/basic_DOM_2/script.js
@@ -0,0 +1,11 @@
+/* 
+Write JavaScript code that adds an event listener to the button.
+
+When the button is clicked, the function should:
+- Retrieve the value from the input field.
+- Create a new <li> element and set its text content to the input field's value.
+- Append the new <li> element to the fruitList <ul>.
+
+Ensure the input field is cleared after adding the fruit to the list.
+
+*/
