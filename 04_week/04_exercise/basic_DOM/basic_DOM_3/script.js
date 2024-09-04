e82bc6a50b3bd172582fa1f22c93c0713d4524d0
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-09-04 14:38:33 +0300

    add task three DOM

---
+++ 04_week/04_exercise/basic_DOM/basic_DOM_3/script.js
@@ -0,0 +1,21 @@
+/* 
+Create functionality where an input field's value is continuously displayed and updated in a dedicated area on the page as the user types, using the onchange event.*/
+
+/* 
+
+Instructions:
+
+HTML:
+- Make a basic HTML
+- Include an <input type="text"> element where the user can enter text.
+- Add a <div> element with an ID (e.g., id="displayText"), which will be used to display the input field's value.
+- Remember to connect HTML and JS files
+
+JavaScript:
+
+Write JavaScript code that adds an event listener to the input field, specifically listening for the change event. 
+
+The event listener's function should:
+- Retrieve the current value from the input field.
+- Update the textContent of the displayText <div> to match the input field's value.
+- To have the display update more dynamically (as the user types), consider using the input event instead of or in addition to the change event.  */
