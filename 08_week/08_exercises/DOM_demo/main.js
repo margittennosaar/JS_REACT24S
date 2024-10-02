0446f1f9344a785c6fe2a4d034bd8f26df1914b9
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-10-02 12:04:28 +0300

    add changes for hero and header background

--- 08_week/08_exercises/DOM_demo/main.js
+++ 08_week/08_exercises/DOM_demo/main.js
@@ -1,6 +1,20 @@
 const backToTop = document.querySelector('#backTop');
 
 backToTop.addEventListener('click', () => {
-    document.body.scrollTop = 200;
-    document.documentElement.scrollTop = 200
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0
 })
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.querySelector('header').style.backgroundColor = "green";
+        document.querySelector('header .logo h1').style.fontSize = "12px";
+        document.querySelector('header').style.height = "50px";
+    } else {
+        document.querySelector('header').style.backgroundColor = "transparent";
+        document.querySelector('header .logo h1').style.fontSize = "32px";
+        document.querySelector('header').style.height = "100px";
+    }
+}
